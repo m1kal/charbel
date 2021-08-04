c58d3452d0d9488992b909d7f5d04d370f4ae3d1
m1kal <mikal82@protonmail.com>
2021-08-04 21:55:11 +0200

    Added cond* to produce if-else

---
+++ src/charbel/expressions.clj
@@ -0,0 +1,76 @@
+(ns charbel.expressions)
+
+(defn from-intermediate [value]
+  (if (keyword? value) (symbol value) (if (number? value) value "-unknown-type-")))
+
+(declare expression)
+
+(defmulti complex-expression (fn [expr env] (first expr)))
+
+(defmethod complex-expression :select [[a & b] env]
+  {:result (str (:result (expression (first b) env))
+                "[" (second b) (if (= 2 (count b)) "]" (str ":" (last b) "]")))
+   :width  (if (= 2 (count b)) 1 (inc (- (second b) (last b))))})
+
+(defmethod complex-expression :+ [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " + " (:result e2)) :width (inc (max (:width e1) (:width e2)))}))
+
+(defmethod complex-expression :- [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " - " (:result e2)) :width (max (:width e1) (:width e2))}))
+
+(defmethod complex-expression :* [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " * " (:result e2)) :width (+ (:width e1) (:width e2))}))
+
+(defmethod complex-expression :inc [[a x & r] env]
+  (let [e1 (expression x env)]
+    {:result (str (:result e1) " + 1") :width (inc (:width e1))}))
+
+(defmethod complex-expression :dec [[a x & r] env]
+  (let [e1 (expression x env)]
+    {:result (str (:result e1) " - 1") :width (:width e1)}))
+
+(defmethod complex-expression :bit-and [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " & " (:result e2)) :width (max (:width e1) (:width e2))}))
+
+(defmethod complex-expression :bit-or [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " | " (:result e2)) :width (max (:width e1) (:width e2))}))
+
+(defmethod complex-expression :bit-xor [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " ^ " (:result e2)) :width (max (:width e1) (:width e2))}))
+
+(defmethod complex-expression :if [[a x y z & r] env]
+  (let [e1 (expression x env) e2 (expression y env) e3 (expression z env)]
+    {:result (str (:result e1) " ? " (:result e2) " : " (:result e3))
+     :width  (max (:width e2) (:width e3))}))
+
+(defmethod complex-expression := [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " == " (:result e2)) :width 1}))
+
+(defmethod complex-expression :mod [[a x y & r] env]
+  (let [e1 (expression x env) e2 (expression y env)]
+    {:result (str (:result e1) " % " (:result e2)) :width (:width e2)}))
+
+(defmethod complex-expression :width [[a w x & r] env]
+  (let [e1 (expression w env) e2 (expression x env)]
+    {:result (str (:result e2)) :width (:result e1)}))
+
+(defmethod complex-expression :default [form env]
+  {:result (str "unnkown " (first form)) :width 32})
+
+(defn expression
+  ([x] (expression x {}))
+  ([form env]
+   (cond
+     (number? form) {:result form :width (inc (int (Math/floor (/ (Math/log (max form 1)) (Math/log 2)))))}
+     (keyword? form) {:result (str (from-intermediate form)) :width (or (env form) 32)}
+     (or (vector? form) (seq? form))
+     (let [e (complex-expression form env)]
+       {:result (str "(" (:result e) ")") :width (:width e)})
+     :else {:result "unknown" :width "unknown"})))
--- src/charbel/synthesis.clj
+++ src/charbel/synthesis.clj
@@ -1,80 +1,6 @@
 (ns charbel.synthesis
-  (:require [charbel.analysis :refer :all]))
-
-(defn from-intermediate [value]
-  (if (keyword? value) (symbol value) (if (number? value) value "-unknown-type-")))
-
-(declare expression)
-
-(defmulti complex-expression (fn [expr env] (first expr)))
-
-(defmethod complex-expression :select [[a & b] env]
-  {:result (str (:result (expression (first b) env))
-                "[" (second b) (if (= 2 (count b)) "]" (str ":" (last b) "]")))
-   :width  (if (= 2 (count b)) 1 (inc (- (second b) (last b))))})
-
-(defmethod complex-expression :+ [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " + " (:result e2)) :width (inc (max (:width e1) (:width e2)))}))
-
-(defmethod complex-expression :- [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " - " (:result e2)) :width (max (:width e1) (:width e2))}))
-
-(defmethod complex-expression :* [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " * " (:result e2)) :width (+ (:width e1) (:width e2))}))
-
-(defmethod complex-expression :inc [[a x & r] env]
-  (let [e1 (expression x env)]
-    {:result (str (:result e1) " + 1") :width (inc (:width e1))}))
-
-(defmethod complex-expression :dec [[a x & r] env]
-  (let [e1 (expression x env)]
-    {:result (str (:result e1) " - 1") :width (:width e1)}))
-
-(defmethod complex-expression :bit-and [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " & " (:result e2)) :width (max (:width e1) (:width e2))}))
-
-(defmethod complex-expression :bit-or [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " | " (:result e2)) :width (max (:width e1) (:width e2))}))
-
-(defmethod complex-expression :bit-xor [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " ^ " (:result e2)) :width (max (:width e1) (:width e2))}))
-
-(defmethod complex-expression :if [[a x y z & r] env]
-  (let [e1 (expression x env) e2 (expression y env) e3 (expression z env)]
-    {:result (str (:result e1) " ? " (:result e2) " : " (:result e3))
-     :width  (max (:width e2) (:width e3))}))
-
-(defmethod complex-expression := [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " == " (:result e2)) :width 1}))
-
-(defmethod complex-expression :mod [[a x y & r] env]
-  (let [e1 (expression x env) e2 (expression y env)]
-    {:result (str (:result e1) " % " (:result e2)) :width (:width e2)}))
-
-(defmethod complex-expression :width [[a w x & r] env]
-  (let [e1 (expression w env) e2 (expression x env)]
-    {:result (str (:result e2)) :width (:result e1)}))
-
-(defmethod complex-expression :default [form env]
-  {:result (str "unnkown " (first form)) :width 32})
-
-(defn expression
-  ([x] (expression x {}))
-  ([form env]
-   (cond
-     (number? form) {:result form :width (inc (int (Math/floor (/ (Math/log (max form 1)) (Math/log 2)))))}
-     (keyword? form) {:result (str (from-intermediate form)) :width (or (env form) 32)}
-     (or (vector? form) (seq? form))
-     (let [e (complex-expression form env)]
-       {:result (str "(" (:result e) ")") :width (:width e)})
-     :else {:result "unknown" :width "unknown"})))
+  (:require [charbel.analysis :refer :all]
+            [charbel.expressions :refer :all]))
 
 (defn build-clocks [clocks]
   (let [hm (apply hash-map (flatten clocks))
@@ -106,6 +32,16 @@
 
 (defn build-element [element clocks]
   (condp = (first element)
+    :cond*
+    (str
+      "always @(*)\n"
+      (clojure.string/join "\n else"
+                           (map
+                             (fn [[c v]] (str " if " (:result (expression c)) "\n  "
+                                              (symbol (second element)) " = " (:result (expression v))))
+                             (partition 2 (drop 2 element))))
+      (if (= 1 (mod (count element) 2)) (str "\n else\n " (symbol (second element)) " = " (:result (expression (last element)))) "")
+      "\n")
     :register
     (str
       "always @(posedge " (symbol (:clk clocks)) ")\n"
@@ -138,6 +74,7 @@
                  :ports  [[:in :a 16] [:in :b 16] [:out :c 16]],
                  :body   [[:register :dout [:+ :a :b]]
                           [:assign :unused [:+ 3 17]]
+                          [:cond* :if_else_register [:= :x 1] 35 [:= :x 2] 36 37]
                           [:assign :c [:select :dout 16 0]]]}]
     (build example))
 
