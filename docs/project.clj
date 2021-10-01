b71bda03dd8fdc7ca89042dc18a249bc07fd2352
m1kal <mikal82@protonmail.com>
2021-10-01 14:15:45 +0200

    Added web interface using ClojureScript

---
+++ docs/project.clj
@@ -0,0 +1,36 @@
+(defproject charbel-cljs "0.1.0-SNAPSHOT"
+  :description "A project demonstrating use of Charbel with Clojurescript"
+  :url "https://github.com/m1kal/charbel"
+  :license {:name "MIT"
+            :url ""}
+
+  :min-lein-version "2.9.1"
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.773"]
+                 [reagent "1.1.0"]
+                 [cljsjs/react "17.0.2-0"]
+                 [cljsjs/react-dom "17.0.2-0"]
+                 [com.github.m1kal/charbel "0.1.6"]]
+
+  :plugins [[lein-figwheel "0.5.20"]
+            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+                :figwheel true
+                :compiler {:main charbel-cljs.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/charbel_cljs.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/charbel_cljs.js"
+                           :main charbel-cljs.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+  )
---
+++ docs/src/charbel_cljs/core.cljs
@@ -0,0 +1,44 @@
+(ns charbel-cljs.core
+  (:require [charbel.core :refer [module-from-string build]]
+            [reagent.core :as r]
+            [reagent.dom :as d]))
+
+(def example
+  (str
+    "(module delay_line\n        {:clocks [[clk]] :parameters [WIDTH 32]}\n"
+    "        [[:in data WIDTH]    [:in valid 1]\n        [:out data_o WIDTH] [:out valid_o 1]]\n"
+    "        (register data_d data)\n        (register valid_d (init 0 valid))\n"
+    "        (assign valid_o valid_d)\n        (assign data_o data_d))"))
+
+(defonce input-form (r/atom example))
+(def output-form (r/atom "<empty>"))
+
+(defn main []
+  [:div {:style {:background-color "rgb(200,240,200)" :width 1280 :padding-left 20 :padding-top 1 :padding-bottom 20}}
+   [:h1 "Charbel example"]
+   [:div "Write your code below"]
+    [:table
+      [:tbody
+        [:tr
+           [:td 
+             [:textarea
+               {:id "i" :style {:width 600 :height 400} :value @input-form
+                :on-change #(reset! input-form (.-value (.-target %)))}]]
+          [:td
+            {:style {:position "relative" :left 30 :width 600
+                     :vertical-align "top" :background-color "rgb(200,220,220)"}}
+            [:pre @output-form]]]
+        [:tr
+          [:td
+            [:button
+              {:id "run" :on-click #(reset! output-form (build (module-from-string @input-form)))}
+              "Convert"]]
+          [:td ""]]]]])
+   
+(defn mount-root []
+  (d/render [main] (.getElementById js/document "main")))
+
+(defn init! []
+  (mount-root))
+
+(init!)
